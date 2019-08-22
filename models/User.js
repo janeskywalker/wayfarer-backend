4477b8fadbb3e5b3f753a1efbb657b2796c14476
Jane Skywalker <janehuizeng@gmail.com>
2019-08-22 11:00:47 -0700

    creating User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  signup_date: {
+    type: Date,
+    default: Date.now,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema); // collection name 
+
+module.exports = User;
