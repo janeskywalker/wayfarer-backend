fffed10800f0aef715d89a942b2c637669434065
alexiliev <iliev.alex@outlook.com>
2019-08-30 16:47:01 -0700

    adding MONGODB_URI

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/auth-api-sei3';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth-api-sei3';
+    
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
